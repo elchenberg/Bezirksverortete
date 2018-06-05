aaed2c6f824d6eec53ebb742e535a470a62e1485
elchenberg <elchenberg@users.noreply.github.com>
2018-06-05 16:58:38 +0200

    Update CSS and HTML Due to Changes in Table Filtering and Sorting

--- htmlwriter.py
+++ htmlwriter.py
@@ -12,37 +12,36 @@ HTML = """<!DOCTYPE html>
 <meta charset=UTF-8>
 <meta content="width=device-width,initial-scale=1" name=viewport>
 <title>Finde Deine Berliner Bezirksverordneten</title>
-<style>html{font-family:Montserrat,Verdana,Geneva,sans-serif;overflow-wrap:break-word;hyphens:auto;line-height:1.6}body{max-width:720px;margin:0 auto;padding:2%}h1{font-family:Merriweather,Georgia,serif}input{padding:.5em;margin-top:1em}a{text-decoration:none;color:#158}a:hover{text-decoration:underline}table{width:100%;border-collapse:collapse;overflow-wrap:normal;hyphens:manual;margin-top:1em}td,th{border-bottom:1px solid #ddd;padding:.5em .2em;text-align:left}</style>
+<style>html{font-family:Montserrat,Verdana,Geneva,sans-serif;line-height:1.6}body{max-width:720px;margin:0 auto;padding:2%;box-sizing:border-box;hyphens:auto}h1{font-family:Merriweather,Georgia,serif;font-size:1.8em}a{text-decoration:none;color:#158}a:hover{text-decoration:underline}table{width:100%;max-width:100%;border-collapse:collapse;word-wrap:normal;overflow-wrap:normal;hyphens:auto;margin-top:1em;box-sizing:border-box}td,th{border-bottom:1px solid #ddd;text-align:left;padding:.5em .2em}tr td:nth-child(3){hyphens:none}.sort:hover{cursor:pointer}.search{box-sizing:border-box;width:100%}.indicator{color:#555}.desc .indicator,.asc .indicator{color:#000}@media screen and (max-width:420px){th,td{display:inline-block;border-width:0}td:last-child{width:100%;border-width:1px}}label{display:none}table caption::before{content:"Tabelle: "}caption{font-size:smaller;color:#555}</style>
 </head>
-<body onload="autofillPLZ()">
+<body>
 <header>
 <h1>Finde Deine Berliner Bezirksverordneten</h1>
-<p>In der untenstehenden Tabelle sind alle Berliner Bezirksverordneten mit der Postleitzahl ihrer Wohnanschrift zu finden. Diese Postleitzahlen sind <em>vom Sommer 2016</em> und können daher inzwischen veraltet sein. Sie wurden damals von der Berliner Landeswahlleiterin mit den offiziellen Wahlvorschlägen der Parteien veröffentlicht.</p>
+<p>In der untenstehenden Tabelle sind alle Berliner Bezirksverordneten mit der Postleitzahl ihrer Wohnanschrift zu finden. Diese Postleitzahlen sind vom Sommer 2016 und können daher inzwischen veraltet sein. Sie wurden damals von der Berliner Landeswahlleiterin mit den offiziellen Wahlvorschlägen der Parteien veröffentlicht.</p>
 <p>Mit einem Klick auf den Namen der Bezirksverordneten gelangt man zu deren Internetseite im Informationssystem der Bezirksverordnetenversammlung, wo die Ausschussmitgliedschaften und oft auch Kontaktdaten zu finden sind.</p>
 <p><small>Wer Fehler findet oder mir Rückmeldung geben möchte, erreicht mich per Email <em>helge at elchenberg dot me</em> oder Twitter <a href=https://twitter.com/elchenberg>twitter.com/elchenberg</a>. Die Daten der Tabelle können <a href="$filename">als CSV heruntergeladen werden</a>.</small></p>
 </header>
-<script>
-document.write('<input type="number" id="inputPLZ" onkeyup="filterPLZ()" oninput="filterPLZ()" placeholder="PLZ" maxlength="5" min="10115" max="14199">')
-</script>
-<table>
+<table id="data">
+<caption>Alle Mitglieder der Berliner Bezirksverordnetenversammlungen</caption>
 <thead>
 <tr>
-<th>PLZ</th>
-<th>Name</th>
-<th>Fraktion</th>
-<th>Bezirksverordneten­versammlung</th>
+<th><span class="sort" data-sort="plz"><abbr title="Postleitzahl">PLZ</abbr></span></th>
+<th><span class="sort" data-sort="name">Name</span></th>
+<th><span class="sort" data-sort="fraktion">Fraktion</span></th>
+<th><span class="sort" data-sort="bvv"><abbr title="Bezirksverordnetenversammlung">BVV</abbr></span></th>
 </tr>
 </thead>
-<tbody>
+<tbody class="list">
 $tbody
 </tbody>
 </table>
-<script>function autofillPLZ(){var input=window.location.hash.substr(1);if(!isNaN(input)&&input.length<=5){document.getElementById("inputPLZ").value=input;filterPLZ()}}function filterPLZ(){var input,tbody,tr,td,i;input=document.getElementById("inputPLZ").value;tbody=document.getElementsByTagName("tbody")[0];tr=tbody.getElementsByTagName("tr");if(!isNaN(input)&&input.length<=5){for(i=0;i<tr.length;i++){td=tr[i].getElementsByTagName("td")[0];if(td){if(td.innerHTML.startsWith(input)){tr[i].style.display=""}else{tr[i].style.display="none"}}}}}</script>
+<script src="js/list-1.5.0.min.js"></script>
+<script src="js/main.js"></script>
 </body>
 </html>"""
-ROWLINK = ('<tr><td>%s</td><td><a href="%s">%s</a></td><td>%s</td>'
-           '<td>%s</td></tr>\n')
-ROW = '<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>\n'
+ROWLINK = ('<tr><td class="plz">%s</td><td><a class="name" href="%s">%s</a></td><td class="fraktion">%s</td>'
+           '<td class="bvv">%s</td></tr>\n')
+ROW = '<tr><td class="plz">%s</td><td class="name">%s</td><td class="fraktion">%s</td><td class="bvv">%s</td></tr>\n'
 def main():
     with open(COUNCILLORSCSV, 'r') as csvfile:
         csvrows = csv.DictReader(csvfile)
@@ -70,11 +69,11 @@ def main():
                 row['FRAKTION'],
                 row['BEZIRK']
             )
-
     html = string.Template(HTML)
-    html = html.safe_substitute({'filename': COUNCILLORSCSV,
-                                 'tbody': tbody})
-    #html = html.replace('\n', '')
+    html = html.safe_substitute({
+        'filename': COUNCILLORSCSV,
+        'tbody': tbody.strip(),
+    })
     with open('index.html', 'w') as htmlfile:
         htmlfile.write(html)
 if __name__ == '__main__':
